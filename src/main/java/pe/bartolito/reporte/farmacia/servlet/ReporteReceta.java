e44f00a87e37837fce39597ba45edc32247507ba
carloschinga <chinga_carlos@hotmail.com>
2025-08-29 10:55:38 -0500

    Estable 2025-08-29

--- src/main/java/pe/bartolito/reporte/farmacia/servlet/ReporteReceta.java
+++ src/main/java/pe/bartolito/reporte/farmacia/servlet/ReporteReceta.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package servlet;
+package pe.bartolito.reporte.farmacia.servlet;
 
 import dao.JpaPadre;
 import java.io.IOException;
